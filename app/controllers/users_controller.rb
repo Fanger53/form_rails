33288775695c1da6515b383ee03a931fd8b188ec
David Leonardo <fanger53@gmail.com>
2020-12-07 10:12:29 -0500

    add create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
     end
     
     def create
+        @user = User.new(user_params)
+
+  if @user.save
+    redirect_to new_user_path
+  else
+    render :new
+  end
     end
 
     def user_params
