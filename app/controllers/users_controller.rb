f2afbd8a8e32646160d82541d649fd2735138866
A B M Asadullah <abmasadullah@gmail.com>
2020-12-07 21:58:59 +0600

    Edit form add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,22 @@ class UsersController < ApplicationController
   end
     end
 
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
     end
+  end
+
+  private
+    
+  def user_params
+      params.require(:user).permit(:username, :email, :password)
+  end
 end
